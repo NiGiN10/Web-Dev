dbda36fd7b03cd760f35559a6f0173415d9dba71
NiGiN10 <56646775+NiGiN10@users.noreply.github.com>
2021-05-17 20:26:44 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,11 @@
+function purple_button(){
+    document.body.style.background="violet"
+}
+
+function green_button(){
+    document.body.style.background="lime"
+}
+
+function yellow_button(){
+    document.body.style.background="yellow"
+}
